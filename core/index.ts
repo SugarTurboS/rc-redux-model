586ccd3f5d054392ba4734d6325feb4f964c5125
pengdaokuan <pengdaokuan@cvte.com>
2020-08-19 20:51:46 +0800

    [feature][what]重新捋逻辑，ts重写

---
+++ core/index.ts
@@ -0,0 +1,83 @@
+import invariant from 'invariant'
+import middleware from './middleware'
+import { IParentModelProps, IModelProps } from './interface'
+/**
+ * @desc RcReduxModel
+ * @property {Object} models - 所有的 models
+ * @property {Object} reducers - 所有 model 下的 reducers，整合在一起，用于 redux.combineReducers
+ * @property {Array} thunk - 自己实现的 thunk 中间件，对 dispatch 的增强
+ */
+class RcReduxModel {
+  public models: IParentModelProps
+  public reducers: any
+  public thunk: any
+
+  public constructor(models: Array<IModelProps>) {
+    this.models = {}
+    this.reducers = {}
+    this.thunk = []
+    this.start(models)
+  }
+
+  public start(models: Array<IModelProps>) {
+    models.forEach((model: IModelProps) => {
+      // 1. 对每一个 model 都进行检查
+      this.registerModel(model, models)
+      // 2. 生成 reducers 纯函数
+      this.reducers[model.namespace] = this.registerReducers(model)
+    })
+    // 3. 生成 thunk 中间件
+    this.thunk = middleware(this.models)
+  }
+
+  public registerModel(model: IModelProps, models: Array<IModelProps>) {
+    // 1.1 检查有无命名空间
+    invariant(model.namespace, `model's namespace is undefined`)
+    // 1.2 检查命名空间必须是字符串
+    invariant(
+      typeof model.namespace === 'string',
+      `model's namespace should be string, but got ${typeof model.namespace}`
+    )
+    // 1.3 检查是否出现重名现象
+    const duplicateModel = models.filter(
+      (mod: IModelProps) => mod.namespace === model.namespace
+    )
+    invariant(
+      duplicateModel.length <= 1,
+      `model's namespace should be unique, but now got the same namespace length = ${duplicateModel.length}, with the same namespace is ${model.namespace}`
+    )
+    // 1.4 注册每一个 model, 添加至 this.models
+    if (!this.models[model.namespace]) {
+      this.models[model.namespace] = model
+    }
+  }
+
+  public registerReducers(model: IModelProps) {
+    const { namespace, state, reducers } = model
+    // 2.1 检查 reducers
+    invariant(reducers, `model's reducers must be defined, but got undefined`)
+
+    // 2.2 得到所有 reducers 中的 action
+    const reducersActionTypes = Object.keys(reducers)
+
+    // 2.2 reducers 是一个纯函数，function(state, action) {}
+    return (storeState: any, storeAction: any) => {
+      const newState = storeState || state
+      // 2.3 对 action 进行处理，规定 action.type 都是 namespace/actionName 的格式
+      const reducersActionKeys = storeAction.type.split('/')
+
+      const reducersActionModelName = reducersActionKeys[0]
+      const reducersActionSelfName = reducersActionKeys[1]
+
+      // 2.3.1 如果不是当前的 model
+      if (reducersActionModelName !== namespace) return newState
+      // 2.3.2 如果在 reducers 中存在这个 action
+      if (reducersActionTypes.includes(reducersActionSelfName)) {
+        return reducers[reducersActionSelfName](newState, storeAction.type)
+      }
+      return newState
+    }
+  }
+}
+
+export default RcReduxModel
