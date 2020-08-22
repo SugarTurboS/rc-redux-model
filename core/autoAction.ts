25ef5bb6e5626b6357701f7ac5e33cf89fcbf80a
pengdaokuan <pengdaokuan@cvte.com>
2020-08-22 12:16:16 +0800

    [feature][what]完成初步版本，提供默认对外导出action

---
+++ core/autoAction.ts
@@ -0,0 +1,98 @@
+import { IModelProps } from './interface'
+
+/**
+ * @desc 对每一个 model 自动注册 action 与 reducers
+ * @summary 每一个 state 字段，都提供一个修改的 action
+ * @param {object} model
+ * @returns {object} newModel
+ */
+const autoAction = (model: IModelProps) => {
+  let _wrapAutoAction = {}
+  let _wrapAutoReducers = {}
+  const { namespace, state = {}, action = {}, reducers = {} } = model
+  const stateKeys = Object.keys(state)
+
+  // 如果是空state，那么直接返回 model
+  if (stateKeys.length === 0) return model
+
+  // 遍历 state，给每一个值都自动注册修改此state的action
+  stateKeys.forEach((key: string) => {
+    const actionType = generateActionType(namespace, key)
+    if (!_wrapAutoAction[`change${key}`]) {
+      _wrapAutoAction[`change${key}`] = registerAction(actionType)
+    }
+    if (!_wrapAutoReducers[`${actionType}`]) {
+      _wrapAutoReducers[`${actionType}`] = registerReducers()
+    }
+  })
+  // 允许重名情况下覆盖，以用户定义的为主
+  _wrapAutoAction = { ..._wrapAutoAction, ...action }
+  _wrapAutoReducers = { ..._wrapAutoReducers, ...reducers }
+
+  // 为了防止太多个state对应action，所以提供一个默认的action，根据用户传递的key，策略模式分配到对应的action
+  _wrapAutoAction['setStoreLib'] = generateDefaultAction(namespace)
+  return { ...model, action: _wrapAutoAction, reducers: _wrapAutoReducers }
+}
+
+/**
+ * @desc 生成 action.type
+ * @summary action 发出的 action.type 需对应 reducer 中的 action.type
+ * @param {string} namespace - 命名空间
+ * @param {string} key - 当前的state key值
+ * @returns {string} actionType
+ */
+const generateActionType = (namespace: string, key: string): string => {
+  return `${namespace.toUpperCase()}_STORE_LIB_${key.toUpperCase()}`
+}
+
+/**
+ * @desc 自动注册 action
+ * @param {string} actionType
+ * @returns {function} actionFunction
+ */
+const registerAction = (actionType: string): Function => {
+  return ({ currentAction, commit }: any) => {
+    commit({
+      type: actionType,
+      payload: currentAction.payload,
+    })
+  }
+}
+
+/**
+ * @desc 自动注册 reducers
+ * @param {string} namespace - 命名空间
+ * @param {string} key - 当前的state key值
+ */
+const registerReducers = () => {
+  return (state: any, payload: any) => {
+    return {
+      ...state,
+      ...payload,
+    }
+  }
+}
+
+/**
+ * @desc 注册生成默认的action
+ * @summary 使用方式
+ * this.props.dispatch({
+ *   type: '[model.namespace]/setStoreLib',
+ *   payload: {
+ *     key: [model.state.key]
+ *     value: [your values]
+ *   }
+ * })
+ */
+const generateDefaultAction = (namespace: string) => {
+  return ({ currentAction, dispatch }: any) => {
+    // 根据key，触发对应的action
+    const keyProps = currentAction.payload && currentAction.payload.key
+    dispatch({
+      type: `${namespace}/change${keyProps}`,
+      payload: currentAction.payload.values,
+    })
+  }
+}
+
+export default autoAction
---
+++ core/middleware.ts
@@ -0,0 +1,96 @@
+import invariant from 'invariant'
+import { IModelProps } from './interface'
+
+// 获取当前这个 actionModel 对应的 model
+const getCurrentModel = (
+  actionModelName: string,
+  models: Array<IModelProps>
+) => {
+  if (models.length === 0) return null
+  const findModel = models.filter(
+    (model: IModelProps) => model.namespace === actionModelName
+  )
+  if (findModel.length > 0) return findModel[0]
+  return null
+}
+
+// 发起一个同步 action ，reducer 接收
+const actionToReducer = (
+  currentModel: IModelProps,
+  actionModelName: string,
+  next: any
+) => {
+  return (reducerAction: any) => {
+    if (currentModel && currentModel.reducers) {
+      if (currentModel.reducers[reducerAction.type]) {
+        next({
+          type: `${actionModelName}/${reducerAction.type}`,
+          payload: reducerAction.payload,
+        })
+      }
+    }
+  }
+}
+
+const callAPI = (dispatch: any) => async (service: any, params: any) => {
+  let result = {}
+  try {
+    result = await service(params)
+  } catch (error) {
+    return Promise.reject(params)
+  }
+
+  return Promise.resolve(result)
+}
+
+const registerMiddleWare = (models: any) => {
+  // middleware 中间件的写法，想了解可参考 : https://cn.redux.js.org/docs/advanced/Middleware.html
+  return ({ dispatch, getState }) => (next: any) => (action: any) => {
+    // 阅读了 redux-thunk 方式，并不适用我们这里
+    // 这里边对 action 进行处理，处理完了，再发到 reducers 中
+    const actionKeyTypes = action.type.split('/')
+    invariant(
+      actionKeyTypes.length <= 2,
+      `dispatch action only accept [namespace/actionName], but got ${action.type}`
+    )
+    // 1. 因为可能存在多个 model/sameActionName，所以需要找到当前的 model
+    const actionModelName = actionKeyTypes[0]
+    const actionSelfName = actionKeyTypes[1]
+    const currentModel = getCurrentModel(actionModelName, models)
+    // 2. 当前 model 是否拥有 action 集，存在则找到对应的 actionSelfName
+    if (currentModel) {
+      const currentModelAction = currentModel.action
+        ? currentModel.action[actionSelfName]
+        : null
+
+      // redux-thunk 中是对其 action 进行类型判断，认为 function 类型的 action 就是异步 action
+      // 而我们在 model.js 文件中对 action 的写法，都是 function 类型，等价于，我们的 action 都是异步的
+      if (currentModelAction && typeof currentModelAction === 'function') {
+        const commitActionToReducer = actionToReducer(
+          currentModel,
+          actionModelName,
+          next
+        )
+        return currentModelAction({
+          dispatch,
+          getState,
+          currentAction: action,
+          commit: commitActionToReducer,
+          call: callAPI(dispatch),
+        })
+      }
+      const commitActionToReducer = actionToReducer(
+        currentModel,
+        actionModelName,
+        next
+      )
+      return commitActionToReducer({
+        ...action,
+        type: actionSelfName,
+      })
+    }
+    return next(action)
+  }
+}
+
+export default registerMiddleWare
