7887e9bd300e9c12df99a2825966a45e7f87519e
pengdaokuan <pengdaokuan@cvte.com>
2020-08-26 09:48:35 +0800

    [feature][what]去掉注释

--- core/autoAction.ts
+++ core/autoAction.ts
@@ -14,10 +14,8 @@ const autoAction = (model: IModelProps) => {
   const { namespace, state = {},  action = {}, reducers = {}, openSeamlessImmutable = false, } = model
   const stateKeys = Object.keys(state)
 
-  // 如果是空state，那么直接返回 model
   if (stateKeys.length === 0) return model
 
-  // 遍历 state，给每一个值都自动注册修改此state的action
   stateKeys.forEach((key: string) => {
     const actionType = generateActionType(namespace, key)
     if (!_wrapAutoAction[`set${key}`]) {
@@ -27,12 +25,10 @@ const autoAction = (model: IModelProps) => {
       _wrapAutoReducers[`${actionType}`] = generateReducers(key, openSeamlessImmutable)
     }
   })
-  // 允许重名情况下覆盖，以用户定义的为主
   _wrapAutoAction = { ..._wrapAutoAction, ...action }
   _wrapAutoReducers = { ..._wrapAutoReducers, ...reducers }
-
-  // 为了防止太多个state对应action，所以提供一个默认的action，根据用户传递的key，策略模式分配到对应的action
   _wrapAutoAction['setStore'] = generateDefaultAction(namespace)
+
   return { ...model, action: _wrapAutoAction, reducers: _wrapAutoReducers }
 }
 
@@ -68,7 +64,6 @@ const registerAction = (actionType: string): Function => {
  */
 const generateReducers = (key: string, openSeamlessImmutable: boolean) => {
   return (state: any, payload: any) => {
-    // 判断类型是否一致，如 model.state 里边对某个state值类型是string，但是想修改为object，这也是不合理的
     const prevStateKeyType = Object.prototype.toString.call(state[key])
     const nextStateKeyType = Object.prototype.toString.call(payload)
     invariant(
@@ -112,10 +107,8 @@ const generateDefaultAction = (namespace: string) => {
   return ({ currentAction, getState, dispatch }: any) => {
     const currentModelState = getState()[namespace]
     const stateKeys = Object.keys(currentModelState)
-    // 根据key，触发对应的action
     const keyProps = currentAction.payload && currentAction.payload.key
 
-    // 如果state值不存在，如 state {} 中只有a、b，然后通过commit想添加个c，这是不合理的
     invariant(
       stateKeys.includes(keyProps),
       `you didn't define the [${keyProps}] in the model.state, please check for correctness`
